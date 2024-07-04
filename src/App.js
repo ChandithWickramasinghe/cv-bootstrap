6beb6bbe78faaa3b5ed66378ec558ed7de474bb6
ChandithWickramasinghe <chandith2014@gmail.com>
2024-07-04 14:42:01 +0530

    Completed one

---
+++ src/App.js
@@ -0,0 +1,185 @@
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App2() {
+  return (
+    <div className="container-fluid">
+
+      <section className="row">
+        
+        <section1 className="col-4 bg-dark text-white">
+          <img src="CDE_6385.jpg" className="ml-5 mt-3 rounded-circle mb-3 border border-white border-5" alt="PD" width="200" height="200" />
+          
+          <div className="mb-4">
+            <h5 className="font-weight-bold">CONTACT</h5>
+            <hr className="hrw" />
+            <p className="text-left">
+              <div className="d-flex align-items-center mb-2">
+                <img src="https://www.citypng.com/public/uploads/preview/-121610564853gg73ucwrdk.png" alt="Phone" width="30" height="30" />
+                <span className="ml-2">+94 76 125 2841</span>
+              </div>
+              <div className="d-flex align-items-center mb-2">
+                <img src="https://th.bing.com/th/id/OIP.-VJgyMiLg4SEfa--HBpwwwHaHa?w=512&h=512&rs=1&pid=ImgDetMain" alt="Email" width="30" height="30" />
+                <span className="ml-2">chandith2014@gmail.com</span>
+              </div>
+              <div className="d-flex align-items-center mb-2">
+                <img src="https://static.vecteezy.com/system/resources/previews/000/627/684/large_2x/vector-house-icon-symbol-sign.jpg" alt="Address" width="30" height="30" />
+                <span className="ml-2">163/2, Aluthwewa, A/pelessa</span>
+              </div>
+              <div className="ml-3">Tangalle, Sri Lanka</div>
+            </p>
+          </div>
+          
+          <div className="mb-4">
+            <h5 className="font-weight-bold">TECHNICAL SKILLS</h5>
+            <hr className="hrw" />
+            <ul>
+              <li>JAVA</li>
+              <li>PYTHON</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>C#</li>
+              <li>SQL</li>
+              <li>REACT</li>
+            </ul>
+          </div>
+          
+          <div className="mb-4">
+            <h5 className="font-weight-bold">PERSONAL SKILLS</h5>
+            <hr className="hrw" />
+            <ul>
+              <li>Team Work</li>
+              <li>Time Management</li>
+              <li>Leadership</li>
+              <li>Verbal and Written Communication</li>
+            </ul>
+          </div>
+          
+          <div className="mb-4">
+            <h5 className="font-weight-bold">LANGUAGE</h5>
+            <hr className="hrw" />
+            <ul>
+              <li>English</li>
+              <li>Sinhala</li>
+            </ul>
+          </div>
+          
+          <div className="mb-4">
+            <h5 className="font-weight-bold">REFEREES</h5>
+            <hr className="hrw" />
+            <ul>
+              <li>
+                <p>
+                  Mr. KAMAL D WERAHARA<br />
+                  Teacher<br />
+                  Phone: +94 71 3402121<br />
+                  Email: kamaldwerahara@gmail.com
+                </p>
+              </li>
+              <li>
+                <p>
+                  Ms. LDTT DE SILVA<br />
+                  Lecturer - KDU<br />
+                  Phone: +94 76 9635871<br />
+                  Email: thumashids@kdu.ac.lk
+                </p>
+              </li>
+            </ul>
+          </div>
+        </section1>
+        
+
+        <section2 className="col-8" bg-white text-dark>
+          <h1 className="text-left font-weight-bolder ml-1 mt-2 mb-2">CHANDITH <br /> WICKRAMASINGHE</h1>
+          <div className="ml-4 font-weight-bold1 mb-3">UNDERGRADUATE</div>
+          
+          <div className="ml-1 mb-4">
+            <h5 className="font-weight-bold">PROFILE</h5>
+            <hr className="hr" />
+            <p>
+              I aspire to leverage my skills and knowledge to make a meaningful impact in the field of IT. I'm with a keen interest to contribute those to reaal-world projects and need to learn the practical organizational knowledge.
+            </p>
+          </div>
+          
+          <div className="ml-1 mb-4">
+            <h5 className="font-weight-bold">EDUCATION</h5>
+            <hr className="hr" />
+            <p>
+              <div className="mb-2">
+              <b>UNDERGRADUATE</b><br />
+              <b>2021 - PRESENT</b><br />
+              </div>
+              GENERAL SIR JOHN KOTELAWALA DEFENCE UNIVERSITY<br />
+              BSc.Hons Information Technology
+              
+            </p>
+            <p>
+              <div className="mb-2">
+              <b>SCHOOL STUDENT</b><br />
+              <b>January 2009 - August 2018</b><br />
+              </div>
+              RUHUNU VIJAYABA COLLEGE, BELIATTA<br />
+              GCE Advanced Level - 2017,2018 (Biological Science)<br />
+              GCE Ordinary Level - 2014
+            </p>
+          </div>
+          
+          <div className="ml-1 mb-4">
+            <h5 className="font-weight-bold">PUBLICATION</h5>
+            <hr className="hr" />
+            <p>
+              <div className="mb-2">
+              <b>A review paper</b><br />
+              </div>
+              A review paper on Cloud computing security issues: Challenges and oppotunities for the 4th students' symposium of Faculty of computing KDU
+            </p>
+          </div>
+          
+          <div className="ml-1 mb-4">
+            <h5 className="font-weight-bold">UNIVERSITY PROJECTS</h5>
+            <hr className="hr" />
+            <p>
+              <div className="mb-2">
+              <b>VEHICLE SERVICE CENTER SYSTEM</b><br />
+              </div>
+              Web application for vehicle service center for the Rapid application development module
+            </p>
+            <p>
+              <div className="mb-2">
+              <b>LEAVE MANAGEMENT SYSTEM FOR KDU SOUTHERN CAMPUS</b><br />
+              </div>
+              System for students leave management as the second-year project (Group)
+            </p>
+          </div>
+          
+          <div className="ml-1 mb-4">
+            <h5 className="font-weight-bold">CERTIFICATES</h5>
+            <hr className="hr" />
+            <p>
+              <b>Completed -</b><br />
+              Programming in Python - 1. Python for Beginners by University of Moratuwa<br />
+              CCNAv7: Introduction to Networks by Cisco Networking Academy<br />
+              IT Essentials: PC Hardware and Software by Cisco Networking Academy<br />
+              CCNAv7: Switching, Routing, and Wireless Essentials by Cisco Networking Academy<br />
+              IoT Fundamentals: Connecting Things by Cisco Networking Academy<br />
+              IoT Fundamentals: IoT Security by Cisco Networking Academy<br />
+              IoT Fundamentals: Big Data & Analytics by Cisco Networking Academy
+            </p>
+            <p>
+              <b>In Progress -</b><br />
+              Trainee- Full Stack Developer by University of Moratuwa<br />
+              Web Development - 1. Web Design for Beginners by University of Moratuwa<br />
+              Project Management Skills by University of Moratuwa
+            </p>
+          </div>
+        </section2>
+
+      </section>
+
+    </div>
+
+  );
+}
+
+export default App2;
